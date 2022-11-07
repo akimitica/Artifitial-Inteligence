7f44a3028c87efcddc82897bc724f5d7d5d12aca
akimitica <akimitica@elfak.rs>
2022-11-07 01:15:11 +0100

    7.a done

--- LP1/P5.py
+++ LP1/P5.py
@@ -6,9 +6,19 @@ def brojel(arg):
         res[x]=len(arg[x]) if type(arg[x]) is list else -1
     return res
 
-print(brojel([[1, 2], [3, 4, 5], 'el', ['1', 1]]))
+##print(brojel([[1, 2], [3, 4, 5], 'el', ['1', 1]]))
 
 
 ##5.b
 
-##def proizvod(arg1,arg2):
+def proizvod(arg1,arg2):
+    res = [None] * len(arg2)
+    
+    for x in range(len(arg1)):
+        sum=0
+        for y in range(len(arg1[x])):
+            sum+=arg1[x][y]
+        res[x]=sum*arg2[x]
+    return list(res)
+
+print(proizvod([[1,2,3],[4,5,6],[7,8,9]], [1,2,3]))
--- LP1/P6.py
+++ LP1/P6.py
@@ -7,9 +7,18 @@ def razlika(arg1,arg2):
             res.append(x)
     return res
 
-print (razlika([1, 4, 6, "2", "6"], [4, 5, "2"]))
+#print (razlika([1, 4, 6, "2", "6"], [4, 5, "2"]))
 
 
 ##6.b
 
-#def objedini(arg1,arg2):
+def objedini(arg1,arg2):
+    kraci = arg1 if len(arg1) < len(arg2) else arg2
+    kraci.extend([0] * abs( len(arg1) - len(arg2)))
+
+    ##return list(zip(arg1,arg2))
+
+    return list(map(lambda x,y:(x,y) if x<y else (y,x),arg1,arg2))
+
+
+print(objedini([1, 7, 2, 4, 5], [2, 5, 2]) )
